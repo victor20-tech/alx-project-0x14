7a3b1bab89869351c07f5354bf9c5983ca5bbeff
victor20-tech <victoradomako10@gmail.com>
2025-10-22 20:43:28 +0000

    feat: implement Loading component and Movies page with movie fetching logic

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import { MovieProps } from "@/interfaces"
+import Image from "next/image"
 
-interface MovieCardProps {
-    // Add props interface here later as needed
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
+  return (
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
+
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
+    </div>
+  )
 }
-
-const MovieCard: React.FC<MovieCardProps> = () => {
-    return (
-        <div className="movie-card">
-            {/* Movie card content will go here */}
-        </div>
-    );
-};
 
 export default MovieCard;
